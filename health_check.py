88fc8bd02f9b931d125e96281437fe15b2c8715f
thenameisappu <krappu203@gmail.com>
2026-04-19 16:53:03 +0530

    Initial commit

--- health_check.py
+++ health_check.py
@@ -79,7 +79,7 @@ def check_db_connection():
     load_dotenv()
     try:
         conn = psycopg2.connect(
-            host=os.getenv("DB_HOST", "postgresql"),
+            host=os.getenv("DB_HOST", "postgresql-database-igcwckskcokscwcgswws880w"),
             database=os.getenv("DB_NAME", "flaskai"),
             user=os.getenv("DB_USER", "postgres"),
             password=os.getenv("DB_PASSWORD", ""),
@@ -116,7 +116,7 @@ def check_db_connection_json() -> dict:
     # Step 1: Connect
     try:
         conn = psycopg2.connect(
-            host=os.getenv("DB_HOST", "postgresql"),
+            host=os.getenv("DB_HOST", "postgresql-database-igcwckskcokscwcgswws880w"),
             database=os.getenv("DB_NAME", "flaskai"),
             user=os.getenv("DB_USER", "postgres"),
             password=os.getenv("DB_PASSWORD", ""),
